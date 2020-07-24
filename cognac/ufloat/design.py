9af87d5b4a7690deccbdc359958c291b0ba55137
Aurélien Ponte <aponte@baliste.local>
2020-07-24 18:30:30 +0200

    design ufloat major overhaul gui

--- cognac/ufloat/design.py
+++ cognac/ufloat/design.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import param
 from bokeh.plotting import figure
+from bokeh.layouts import gridplot
 
 import panel as pn
 #pn.extension('plotly')
@@ -12,151 +13,269 @@ from graphviz import Graph
 rho0 = 1030 # kg/m3
 g = 9.81 # m/s^2
 
+# https://en.wikipedia.org/wiki/D_battery
+D_volume = np.pi*(33.2/2.)**2 * 61.5*1e-9
+D_mass_lithium = 96e-3
+D_mass_alcaline = 136e-3
+
+_items = ['deployment','hull', 'piston', 'battery', 'electronics']
+_colors = {'deployment': 'orange', 
+           'hull': 'cadetblue',
+           'piston': 'gray',
+           'electronics': 'salmon',
+           'battery': 'orange',
+           'total': 'black',
+           }
+
 class ufloat(param.Parameterized):
-        
-    d_depth = param.Number(500, bounds=(10,4000), step=10, 
-                           label='depth', doc="Deployment depth [m]")
-    d_T = param.Number(30, bounds=(1,300), step=1, 
-                       label='T', doc="Deployment time length [days]")
-    d_delta_rho = param.Number(5, bounds=(1,30), step=.5, 
-                               label='delta_rho', doc="Deployment density delta [kg/m^3]")
     
-    h_length = param.Number(0.8, bounds=(.1,2.), step=.02, 
-                            label='length', doc="Hull length [m]")
+    d_depth = param.Number(500, bounds=(10,4000), step=10, 
+                           label='depth [m]', doc="Deployment depth [m]")
+    d_T = param.Number(30, bounds=(1,300), step=1, 
+                       label='T [days]', doc="Deployment time length [days]")
+    d_delta_rho = param.Number(5, bounds=(1,30), step=.5, 
+                               label='delta_rho [kg/m3]', doc="Deployment density delta [kg/m^3]")
+    
+    #h_length = param.Number(0.8, bounds=(.1,2.), step=.02, 
+    #                        label='length [m]', doc="Hull length [m]")
     h_radius = param.Range(default=(1., 50.), bounds=(1.,100.), 
-                           label='radius', doc="Hull radius [cm]")
+                           label='radius [cm]', doc="Hull radius [cm]")
     h_thickness = param.Number(.5, bounds=(.1,2.), step=.1, 
-                               label='thickness', doc="Hull thickness [cm]")
+                               label='thickness [cm]', doc="Hull thickness [cm]")
     h_density = param.Number(2700, bounds=(1000,3000), step=50., 
-                             label='density', doc="Hull density [kg/m^3]")
+                             label='density [kg/m3]', doc="Hull density [kg/m^3]")
 
     p_length = param.Number(.2, bounds=(.1,1.), step=.01, 
                             label='length',doc="Piston length [m]")
     #radius = param.Number(.0095, bounds=(.001,1.), step=.001, doc="Piston radius [m]") # dependent variable
     p_density = param.Number(2700, bounds=(1000,3000), step=50., 
-                             label='density', doc="Piston density [kg/m^3]")
+                             label='density [kg/m3]', doc="Piston density [kg/m^3]")
     p_efficiency = param.Number(.1, bounds=(.01,1.), step=.01, 
-                                label='efficiency', doc="Piston energetic efficiency [1]")
-    p_speed = param.Number(.1, bounds=(.01,10), step=.01, label='speed',
+                                label='efficiency [1]', doc="Piston energetic efficiency [1]")
+    p_speed = param.Number(.1, bounds=(.01,10), step=.01, label='speed [m/h]',
                            doc="Piston speed [m/h]")
 
     e_volume = param.Number(250, bounds=(10,1000), step=10, 
-                            label='volume', doc="Electronics volume [cm^3]")
+                            label='volume [cm3]', doc="Electronics volume [cm3]")
     e_mass = param.Number(.400, bounds=(.1,1.), step=.05,
-                          label='mass', doc="Electronics mass [kg]")
+                          label='mass [kg]', doc="Electronics mass [kg]")
     e_c = param.Number(.1, bounds=(.01,1.), step=.01, 
-                       label='conssumption', doc="Electronics conssumption [W]")
+                       label='conssumption [W]', doc="Electronics conssumption [W]")
 
     #mass = param.Number(3., bounds=(.1,20.), step=.1, doc="Battery mass [kg]") # dependent variable
     b_cell = param.Selector(objects=["D",], label='size', doc="Battery size")
     b_lithium = param.Boolean(True, label='lithium', doc="Battery type")
 
     _params = ['d_depth', 'd_T', 'd_delta_rho',
-               'h_length', 'h_radius', 'h_thickness', 'h_density',
+               'h_radius', 'h_thickness', 'h_density', # 'h_length', 
                'p_length', 'p_density', 'p_efficiency', 'p_speed',
                'e_volume', 'e_mass', 'e_c', 
                'b_cell', 'b_lithium',
               ]
-    _parts = ['deployment', 'hull', 'piston', 'electronics', 'battery']
-    _mapping = {p[0]: p for p in _parts}
+    _scales = {'d_depth': 1., 
+               'd_T': 86400., 
+               'd_delta_rho': 1., #'h_length': 1.,
+               'h_radius': 1e-2, 
+               'h_thickness': 1e-2,
+               'h_density': 1,
+               'p_length': 1.,
+               'p_density': 1., 
+               'p_efficiency': 1.,
+               'p_speed': 1/3600.,
+               'e_volume': 1e-6, 
+               'e_mass': 1., 
+               'e_c': 1., 
+               'b_cell': None, 
+               'b_lithium': None,
+               }
+    #_parts = ['deployment', 'hull', 'piston', 'electronics', 'battery']
+    _mapping = {p[0]: p for p in _items}
     
     def get_part(self, item):
         assert item in self._params
         return self._mapping[item.split('_')[0]]
+    
+    def get_key_and_part(self, item):
+        assert item in self._params
+        item_split = item.split('_')
+        return '_'.join(item_split[1:]), self._mapping[item_split[0]]
 
     def __init__(self, **params):
         super(ufloat, self).__init__(**params)
-                
+
+        # will mirror data in dicts:
+        self.D = {i: {} for i in _items}
+        self.D['total'] = {}
+
         # init figures
-        self.volume_figure=figure(plot_width=800, plot_height=300)
-        #self.figure = figure(x_range=(-1, 10), y_range=(-1, 10))
-        self.volume_renderer = self.volume_figure.line([], [],
-                                                       line_width=3, 
-                                                       legend_label='hull volume [cm^3]',
-                                                      )
+        self.renderers = {v: {} for v in ['volume', 'length', 'mass']}
+        line = lambda f, c, l: f.line([],[], 
+                                      color=c,
+                                      line_width=3,
+                                      legend_label=l
+                                      )
+        _tools = 'pan,wheel_zoom,box_zoom,reset'
+        f_volume = figure(y_axis_label='volume [liters]',
+                          tools=_tools
+                          )
+        _r = self.renderers['volume']
+        for i in ['hull', 'piston', 'battery', 'electronics', 'total']:
+            _r[i] = line(f_volume, _colors[i], i)
         #
-        self.piston_figure=figure(plot_width=800, plot_height=300,
-                                  x_range=self.volume_figure.x_range)
-        self.piston_renderer = self.piston_figure.line([], [],
-                                                       line_width=3,
-                                                       legend_label='piston radius [cm]'
-                                                      )
+        f_length = figure(y_axis_label='length [cm]',
+                          tools=_tools,
+                          x_range=f_volume.x_range
+                          )
+        _r = self.renderers['length']
+        for i in ['hull', 'piston']:
+            _r[i] = line(f_length, _colors[i], i+' radius')
+        _r['hull_length'] = f_length.line([],[],
+                                          color=_colors[i], 
+                                          line_dash='4 4',
+                                          legend_label='hull length')
         #
-        self.battery_figure=figure(plot_width=800, plot_height=300,
-                                   x_range=self.volume_figure.x_range)
-        self.battery_renderer = self.battery_figure.line([], [],
-                                                         line_width=3, 
-                                                         legend_label='battery mass [kg]',
-                                                        )
-        self.battery_figure.xaxis.axis_label = 'hull radius'
-        #x_range=s1.x_range, 
-        #from bokeh.layouts import gridplot
-        #p = gridplot([[s1, s2, s3]], toolbar_location=None)
+        f_mass = figure(y_axis_label='mass [kg/m3]',
+                        tools=_tools,
+                        x_range=f_volume.x_range
+                        )
+        _r = self.renderers['mass']
+        for i in ['battery', 'hull', 'total']:
+            _r[i] = line(f_mass, _colors[i], i)
+        #
+        f_length.xaxis.axis_label = 'hull radius [cm]'
+        f_mass.xaxis.axis_label = 'hull radius [cm]'
+        #x_range=s1.x_range,
+        self.figures = gridplot([[f_volume, f_mass], [f_length,None]], 
+                                plot_width=400, 
+                                plot_height=300,
+                                #toolbar_location=None,
+                                )
         #
         self.update()
     
     #@param.depends('b_cell', 'b_lithium')
-    def b_edensity(self):
+    def _update_battery(self):
+        """ J/kg
+        alcaline: 1.2V*1Ah=1.2Wh pour 136g
+        lithium: 3.6V*9Ah=32.4Wh pour 96g
+        """
         if self.b_cell is "D":
             if self.b_lithium:
-                return 32.4*3600/96e-3
+                density = D_mass_lithium/D_volume
+                edensity = 32.4*3600/D_mass_lithium
             else:
-                return 1.2*3600/136e-3
+                density = D_mass_alcaline/D_volume
+                edensity = 1.2*3600/D_mass_alcaline
+        self.D['battery']['density'] = density
+        self.D['battery']['edensity'] = edensity
 
     @param.depends(*_params)
-    def volume_view(self):
-        return self.volume_figure
+    def variables_view(self):
+        return self.figures
 
-    @param.depends(*_params)
-    def piston_view(self):
-        return self.piston_figure
-    
-    @param.depends(*_params)
-    def battery_view(self):
-        return self.battery_figure
-        
+    def _update_dict_params(self):
+        # push and rescale data in central dict
+        for p in self._params:
+            _k, _p = self.get_key_and_part(p)
+            _d = self.D[_p]
+            if p=='h_radius':
+                _d['radius'] = np.linspace(self.h_radius[0], 
+                                           self.h_radius[1], 100) /1e2
+            elif self._scales[p]:
+                _d[_k] = getattr(self, p) * self._scales[p]
+
+    def _update_line(self, renderer_key, item, variable, x, scale):
+        _d = self.D[item][variable]
+        if isinstance(_d, float):
+            _d = np.ones_like(x)*_d
+        (self.renderers[renderer_key][item]
+         .data_source
+         .data
+         .update({'x': x*1e2, 
+                  'y': _d*scale,
+                  }
+                )
+         )
+
     @param.depends(*_params, watch=True)
     def update(self):
-        #xs, ys = self._get_coords()
         
+        # update data containers
+        self._update_dict_params()
+        self._update_battery()
+        
+        d, h, p, b, e = (self.D[i] for i in _items)
+        t = self.D['total']
+
         # renormalizations
         d_T = self.d_T*86400.
         h_radius = np.linspace(self.h_radius[0], self.h_radius[1], 100) /1e2
         h_thickness = self.h_thickness/1e2
         p_speed = self.p_speed/3600.
+        e_volume = self.e_volume*1e-6
+
+        # solve for volume first
+        gamma = d['delta_rho']/rho0
+        sigma = (gamma/p['length'] 
+                 *rho0*g*d['depth'] 
+                 *p['speed']/p['efficiency']
+                 )
+        h['volume'] = ( (d['T']*e['c']/b['edensity'] + e['mass'])
+                        /( rho0 
+                          - 2*h['thickness']/h['radius']*h['density']
+                          - p['density'] * gamma
+                          - d['T']*sigma/b['edensity']
+                          )
+                       )
+        h['volume'][np.where(h['volume']<0)] = np.NaN
+        
+        # propagate volume 
+        h['length'] = h['volume']/(np.pi*h['radius']**2)
         
         # piston radius
-        gamma = self.d_delta_rho/rho0
-        p_radius = np.sqrt(self.h_length/self.p_length * gamma) * h_radius
+        #gamma = self.d_delta_rho/rho0
+        #p_radius = np.sqrt(self.h_length/self.p_length * gamma) * h_radius
+        p['radius'] = np.sqrt(h['length']/p['length'] * gamma) * h['radius']
         
         # piston conssumption
-        p_c = (rho0 * g * self.d_depth
-                * np.pi * p_radius**2 
-                * p_speed/self.p_efficiency)
+        p['c'] =  (rho0 * g * d['depth']
+                   * np.pi * p['radius']**2 
+                   * p['speed']/p['efficiency']
+                   )
     
         # battery mass
-        b_mass = d_T*(self.e_c+p_c)/self.b_edensity()
+        #self.update_battery()
+        b['mass'] = d['T']*(e['c']+p['c'])/b['edensity']
+        b['volume'] = b['mass']/b['density']
         
-        # hull volume
-        h_volume = ( (b_mass+self.e_mass)
-                     /(rho0 
-                       - 2*h_thickness/h_radius*self.h_density
-                       - self.p_density*gamma
-                     )
-                   )
-        h_volume[np.where(h_volume<0)] = np.NaN
+        
+        # other parameters
+        h['mass'] = h['volume'] * h['density']
+        p['volume'] = np.pi*p['radius']**2*p['length']
+        p['mass'] = p['density']*p['volume']
+        t['mass'] = h['mass'] + p['mass'] + b['mass'] + e['mass']
+        t['volume'] = p['volume'] + b['volume'] + e['volume']
 
-        self.volume_renderer.data_source.data.update({'x': h_radius, 'y': h_volume*1e6})
-        self.piston_renderer.data_source.data.update({'x': h_radius, 'y': p_radius*1e2})
-        self.battery_renderer.data_source.data.update({'x': h_radius, 'y': b_mass})
-            
+        # update plots
+        for _p in ['hull', 'piston', 'battery', 'electronics', 'total']:
+            self._update_line('volume', _p, 'volume', h['radius'], 1e3)
+        #
+        for _p in ['hull', 'piston', ]:
+            self._update_line('length', _p, 'radius', h['radius'], 1e2)
+        (self.renderers['length']['hull_length']
+         .data_source.data.update({'x': h['radius']*1e2, 
+                                   'y': self.D['hull']['length']*1e2}
+                                  )
+         )
+        #
+        for _p in ['hull', 'battery', 'total']:
+            self._update_line('mass', _p, 'mass', h['radius'], 1)
+
     def panel(self):
         w = self._widgets_panel()
         return pn.Column(pn.Row(w[0], w[1], w[2]),
                          pn.Row(w[3],w[4]),
-                         self.volume_view,
-                         self.piston_view,
-                         self.battery_view,
+                         self.variables_view,
                         )
     
     def _widgets_panel(self):
@@ -166,8 +285,7 @@ class ufloat(param.Parameterized):
                           pn.panel(self.param.d_delta_rho)
                          ),
                 pn.Column('### {}'.format('Hull'),
-                          pn.panel(self.param.h_length),
-                          pn.panel(self.param.h_radius),
+                          pn.panel(self.param.h_radius), #pn.panel(self.param.h_length),
                           pn.panel(self.param.h_thickness),
                           pn.panel(self.param.h_density),
                          ),
